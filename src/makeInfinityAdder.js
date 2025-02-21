e62308a35e1395c11d32812ef0f61ca1dd94db1e
vitalii <vitalii.myhailyk.w@gmail.com>
2025-02-21 22:39:41 +0200

    task solved

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let numberSum = 0;
+
+  const adder = (number) => {
+    /*  if (typeof number === 'number') {
+       numberSum += number;
+       result = adder;
+     }else{
+     result = numberSum;
+     numberSum = 0;
+     } */
+    let result = typeof number === 'number' ? adder : numberSum;
+    numberSum = typeof number === 'number' ? numberSum + number : 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
